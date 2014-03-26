92eaf5e73599e66984496d7bae03665b27b21c98
Calder Phillips-Grafflin <calderpg@eliza.(none)>
2014-03-25 20:47:02 -0400

    Updated PCL calls and structure to work with the standalone PCL used in Hydro

--- lidar_aggregator/src/lidar_aggregator_node.cpp
+++ lidar_aggregator/src/lidar_aggregator_node.cpp
@@ -10,7 +10,7 @@
 #include <lidar_aggregator/LidarAggregation.h>
 
 // PCL specific includes
-#include <pcl/ros/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/io.h>
@@ -23,34 +23,57 @@ tf::TransformListener* g_transformer;
 bool LaserAggregationServiceCB(lidar_aggregator::LidarAggregation::Request& req, lidar_aggregator::LidarAggregation::Response& res)
 {
     ROS_INFO("Attempting to aggregate %ld laser scans into a pointcloud", req.Scans.size());
-    sensor_msgs::PointCloud2 full_cloud;
     if (req.Scans.size() > 1)
     {
-        g_laser_projector.transformLaserScanToPointCloud(g_fixed_frame, req.Scans[0], full_cloud, *g_transformer);
+        /* If there are more than one scan, then we need to carefully convert between ROS and PCL pointcloud types on the fly */
+        // Process and convert the first scan
+        sensor_msgs::PointCloud2 temp_ros_cloud;
+        pcl::PCLPointCloud2 pcl_full_cloud;
+        g_laser_projector.transformLaserScanToPointCloud(g_fixed_frame, req.Scans[0], temp_ros_cloud, *g_transformer);
+        pcl_conversions::moveToPCL(temp_ros_cloud, pcl_full_cloud);
+        // Process, convert, and combine together the remaining scans
         for (unsigned int index = 1; index < req.Scans.size(); index++)
         {
-            sensor_msgs::PointCloud2 scan_cloud;
-            g_laser_projector.transformLaserScanToPointCloud(g_fixed_frame, req.Scans[index], scan_cloud, *g_transformer);
-            bool succeded = pcl::concatenatePointCloud(full_cloud, scan_cloud, full_cloud);
+            g_laser_projector.transformLaserScanToPointCloud(g_fixed_frame, req.Scans[index], temp_ros_cloud, *g_transformer);
+            pcl::PCLPointCloud2 temp_pcl_cloud;
+            pcl_conversions::moveToPCL(temp_ros_cloud, temp_pcl_cloud);
+            bool succeded = pcl::concatenatePointCloud(pcl_full_cloud, temp_pcl_cloud, pcl_full_cloud);
             if (!succeded)
             {
                 ROS_ERROR("PCL could not concatenate pointclouds");
             }
         }
+        // Convert the PCL pointcloud to ROS and finish
+        sensor_msgs::PointCloud2 ros_cloud;
+        pcl_conversions::moveFromPCL(pcl_full_cloud, ros_cloud);
+        ros_cloud.header.frame_id = g_fixed_frame;
+        ros_cloud.header.stamp = ros::Time::now();
+        res.Cloud = ros_cloud;
+        g_cloud_publisher.publish(ros_cloud);
+        return true;
     }
-    else if (req.Scans.size() > 0)
+    else if (req.Scans.size() == 1)
     {
-        g_laser_projector.transformLaserScanToPointCloud(g_fixed_frame, req.Scans[0], full_cloud, *g_transformer);
+        /* If there is exactly one scan, we don't need to do any conversions beteen ROS and PCL explicitly */
+        sensor_msgs::PointCloud2 ros_cloud;
+        g_laser_projector.transformLaserScanToPointCloud(g_fixed_frame, req.Scans[0], ros_cloud, *g_transformer);
+        ros_cloud.header.frame_id = g_fixed_frame;
+        ros_cloud.header.stamp = ros::Time::now();
+        res.Cloud = ros_cloud;
+        g_cloud_publisher.publish(ros_cloud);
+        return true;
     }
     else
     {
+        /* If there are no scans at all, warn and return an empty cloud */
         ROS_WARN("No laser scans to aggregate");
+        sensor_msgs::PointCloud2 ros_cloud;
+        ros_cloud.header.frame_id = g_fixed_frame;
+        ros_cloud.header.stamp = ros::Time::now();
+        res.Cloud = ros_cloud;
+        g_cloud_publisher.publish(ros_cloud);
+        return true;
     }
-    full_cloud.header.frame_id = g_fixed_frame;
-    full_cloud.header.stamp = ros::Time::now();
-    res.Cloud = full_cloud;
-    g_cloud_publisher.publish(full_cloud);
-    return true;
 }
 
 int main(int argc, char** argv)
